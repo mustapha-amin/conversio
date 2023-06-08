27125b9a787aab69fb9f33ee9631c2ce1f0f52c8
mustapha amin <mustaphaamin2003@gmail.com>
2023-06-08 01:20:41 -0700

    defined app colors and themes

---
+++ lib/pallette.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color darkScaffoldBgColor = Color(0x6D000000);
+  static const Color lightScaffoldBgColor = Colors.white;
+  static const Color darktextColor = Colors.white;
+  static const Color lighttextColor = Colors.black;
+  static const Color primary = Color(0xFF191970);
+  static const Color accent = Color(0xEA780707);
+}
---
+++ lib/theme.dart
@@ -0,0 +1,21 @@
+import 'package:conversio/pallette.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData themeData(bool isDark, BuildContext context) {
+    return ThemeData(
+      useMaterial3: true,
+      scaffoldBackgroundColor: isDark ? AppColors.darkScaffoldBgColor : AppColors.lightScaffoldBgColor,
+      primaryColor: AppColors.primary,
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.primary,
+          foregroundColor: Colors.white,
+        )
+      ),
+      appBarTheme: const AppBarTheme(
+        elevation: 0.0
+      )
+    );
+  }
+}
